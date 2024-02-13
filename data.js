e639452a5b5ba7911bdef3e42c7f62dc7fe4eb65
Stanislav <9323354@gmail.com>
2024-02-13 20:25:31 +0300

    First commit

---
+++ data.js
@@ -0,0 +1,69 @@
+const dataProducts = [
+    {
+        "id": 1, 
+        "name": "jacket",
+        "brand": "Ellery x M'O Capsule",
+        "price": 52,
+        "currency": "USD",
+        "color": "red",
+        "size": "XL",
+        "quantity": 1,
+        "image": "prod_1.jpg"
+    },
+    {
+        "id": 2,
+        "name": "dress",
+        "brand": "Ellery x M'O Capsule",
+        "price": 52,
+        "currency": "USD",
+        "color": "black",
+        "size": "XL",
+        "quantity": 1,
+        "image": "prod_2.jpg"
+    },
+    {
+        "id": 3, 
+        "name": "hat",
+        "brand": "Ellery x M'O Capsule",
+        "price": 52,
+        "currency": "USD",
+        "color": "red",
+        "size": "XL",
+        "quantity": 1,
+        "image": "prod_3.jpg"
+    },
+    {
+        "id": 4, 
+        "name": "sneakers",
+        "brand": "Ellery x M'O Capsule",
+        "price": 52,
+        "currency": "USD",
+        "color": "red",
+        "size": "XL",
+        "quantity": 1,
+        "image": "prod_4.jpg"
+    },
+    {
+        "id": 5, 
+        "name": "suit",
+        "brand": "Ellery x M'O Capsule",
+        "price": 52,
+        "currency": "USD",
+        "color": "red",
+        "size": "XL",
+        "quantity": 1,
+        "image": "prod_5.jpg"
+    },
+    {
+        "id": 6, 
+        "name": "The jacket",
+        "brand": "Ellery x M'O Capsule",
+        "price": 52,
+        "currency": "USD",
+        "color": "red",
+        "size": "XL",
+        "quantity": 1,
+        "image": "prod_6.jpg"
+    }
+];
+
